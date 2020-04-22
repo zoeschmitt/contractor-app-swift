89ff9332c7e6240c652060076e039e0d7309f6b7
zoeschmitt <zoe4712@gmail.com>
2020-04-21 19:25:37 -0500

    Initial Commit

---
+++ Contractor App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Contractor App
+//
+//  Created by Zoe Schmitt on 4/21/20.
+//  Copyright © 2020 Zoeschmitt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
